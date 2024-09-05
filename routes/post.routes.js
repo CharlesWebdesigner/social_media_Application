77add37971a79b6e8a4e7e7ce84286de617009e1
Charles N <vadizcharo@gmail.com>
2024-09-05 11:16:21 +0300

    add comment like and upgrade post features

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -10,4 +10,16 @@ router
   .route("/api/posts/by/:userId")
   .get(authCtrl.requireSignIn, postCtrl.listByUser);
 router.param("userId", useCtrl.userByID);
+router
+  .route("/api/posts/new/:userId")
+  .post(authCtrl.requireSignIn, postCtrl.create);
+router
+  .route("/api/posts/:postId")
+  .delete(authCtrl.requireSignIn, postCtrl.isPoster, postCtrl.remove);
+router.route("api/posts/like").put(authCtrl.requireSignIn, postCtrl.like);
+router.route("/api/posts/unlike").put(authCtrl.requireSignIn, postCtrl.unlike);
+router
+  .route("/api/posts/comment")
+  .put(authCtrl.requireSignIn, postCtrl.comment);
+router.param("postId", postCtrl.postByID);
 module.exports = router;
