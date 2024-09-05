7277d79183bbc22c432b4b8149939fdb6c9cb003
Charles N <vadizcharo@gmail.com>
2024-09-05 23:07:47 +0300

    delete unused folders

--- config/config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-// const {proPORT, Mongo_url, jwt } = require("../.env");
-const config = {
-  port: process.env.PORT,
-  url: process.env.Mongo_url,
-  jwtSecret: process.env.jwt,
-};
-module.exports = config;
--- routes/auth.routes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const authCtrl = require("../controller/auth.controller");
-const router = express.Router();
-router.route("/auth/signin").post(authCtrl.signIn);
-router.route("/auth/signout").get(authCtrl.signOut);
-module.exports = router;
